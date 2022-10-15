304ecce931f7c00f8fd89a566ad49b3c86415bca
Samuelhimika <96009947+Samuelhimika@users.noreply.github.com>
2022-10-15 21:37:57 +0300

    Add files via upload

---
+++ Assignment 2/Qn5.py
@@ -0,0 +1,16 @@
+
+# Step 1: write a line of code that prompts the user to replace the middle number with an integer number entered by the user.
+hat_list = [1, 2, 3, 4, 5]
+Int_number = int( input ('Enter any integer number of your choice??'))
+hat_list [2] =  Int_number
+print(hat_list)
+
+# Step 2: write a line of code that removes the last element from the list.
+
+del  hat_list[4]
+print(hat_list)
+
+# Step 3: write a line of code that prints the length of the existing list.
+
+lengthOfExistingList=len (hat_list)
+print(lengthOfExistingList)
